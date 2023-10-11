a3f88c2377c0e617a605d5928c92edf18d7a02cf
Ayesha Dorani <ayesha.dorani@kcl.ac.uk>
2023-10-11 15:57:00 +0100

    Exercise 2 complete

--- things/models.py
+++ things/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+class Thing(models):
+    name = models.CharField(
+
+    )
+
+    description = models.CharField(
+
+    )
+
+    quantity = models.IntegerField(
+        
+    )
